b17581c6fa1f2533a4e571eaf77261949b4df3b2
RoshanDalami <roshandalami0@gmail.com>
2023-11-11 08:28:34 +0545

    added active navlink

--- src/app/(admin)/Components/Sidebar.tsx
+++ src/app/(admin)/Components/Sidebar.tsx
@@ -1,51 +1,87 @@
-'use client'
+"use client";
 
-import Link from "next/link"
-import axios from "axios"
-import { useRouter } from "next/navigation"
+import Link from "next/link";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
+export default function Sidebar() {
+  const router = useRouter();
+  const pathname = usePathname();
 
-export default function Sidebar(){
-    const router = useRouter();
+  const logoutHandler = async () => {
+    const response = await axios("api/users/logout");
+    router.push("/");
+    console.log("logged out");
+  };
+  const links = [
+    {
+      id: "0",
+      title: "Dashboard",
+      link: "/Home",
+    },
+    {
+      id: "1",
+      title: "Orders",
+      link: "/orderstodeliver",
+    },
+    {
+      id: "2",
+      title: "Completed Orders",
+      link: "/completedorders",
+    },
+    {
+      id: "3",
+      title: "Add Products",
+      link: "/addproducts",
+    },
+    {
+      id: "4",
+      title: "Add Coupon",
+      link: "/coupon",
+    },
+    {
+      id: "5",
+      title: "Coupon List",
+      link: "/couponlist",
+    },
+  ];
 
-    const logoutHandler = async() =>{
-        const response = await axios('api/users/logout')
-        router.push('/')
-        console.log('logged out')
-    }
-
-    return(
-        <aside className=" fixed top-0 left-0 z-40 w-[16rem] bg-[#322653] h-screen">       
-        <div className="flex flex-col gap-10 p-3" >
-            <div className="flex flex-col items-center justify-center">
-                <h1 className="mt-10 text-4xl text-white underline font-bold">We Ugly</h1>
-                <p className="text-white/40">Admin board</p>
-            </div>
-            <hr  />
-
-            <div className="flex flex-col gap-6 items-center">
-           <Link href={'/Home'} className="text-white py-4 px-[4rem] hover:bg-white/40 rounded-lg" >
-            <h1 className="text-sm">Dashboard</h1>
-           </Link>
-           <Link href={'/orderstodeliver'} className="text-white py-4 px-[4rem] hover:bg-white/40 rounded-lg" >
-            <h1 className="text-sm">Orders  </h1>
-           </Link>
-           <Link href={'/completedorders'} className="text-white py-4 px-[4rem] hover:bg-white/40 rounded-lg" >
-            <h1 className="text-sm">Completed Orders  </h1>
-           </Link>
-           <Link href={'/addproducts'} className="text-white py-4 px-[4rem] hover:bg-white/40 rounded-lg" >
-            <h1 className="text-sm">Add Products</h1>
-           </Link>
-           <Link href={'/coupon'} className="text-white py-4 px-[4rem] hover:bg-white/40 rounded-lg" >
-            <h1 className="text-sm">Add Coupons</h1>
-           </Link>
-           <Link href={'/couponlist'} className="text-white py-4 px-[4rem] hover:bg-white/40 rounded-lg" >
-            <h1 className="text-sm">Coupons List</h1>
-           </Link>
-            </div>
-        <button className="bg-indigo-600 rounded-md text-white px-4 py-2" onClick={logoutHandler} >
-        signout
-        </button>
+  return (
+    <aside className=" fixed top-0 left-0 z-40 w-[16rem] bg-[#322653] h-screen">
+      <div className="flex flex-col gap-10 p-3">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="mt-10 text-4xl text-white underline font-bold">
+            We Ugly
+          </h1>
+          <p className="text-white/40">Admin board</p>
         </div>
-        </aside>
-    )
+        <hr />
+
+        <div className="flex flex-col gap-6 items-center">
+          {links.map((item) => {
+
+            return (
+              <Link
+                href={item.link}
+                className={clsx(
+                  "text-white py-4 px-[4rem] hover:bg-white/40 rounded-lg",
+                  { "bg-white/40": pathname === item.link }
+                )}
+                key={item.id}
+              >
+                <h1 className="text-sm">{item.title}</h1>
+              </Link>
+            );
+          })}
+        </div>
+        <button
+          className="bg-indigo-600 rounded-md text-white px-4 py-2"
+          onClick={logoutHandler}
+        >
+          signout
+        </button>
+      </div>
+    </aside>
+  );
 }
--- src/app/(admin)/couponlist/page.tsx
+++ src/app/(admin)/couponlist/page.tsx
@@ -3,12 +3,11 @@ import React, { useState, useEffect } from "react";
 import { db } from "@/Firebase/config";
 import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
 
-interface CouponList  {
-    code: string,
-    discount:string,
-    id:string
+interface CouponList {
+  code: string;
+  discount: string;
+  id: string;
 }
-
 
 export default function CouponList() {
   const [couponList, setCouponList] = useState([{}]);
@@ -34,31 +33,32 @@ export default function CouponList() {
   };
 
   return (
-  <div className="flex items-center  justify-center flex-col  ">
-    <h1 className="text-3xl font-bold">
-        Active Coupons
-    </h1>
+    <div className="flex items-center  justify-center flex-col  ">
+      <h1 className="text-3xl font-bold">Active Coupons</h1>
 
-<div className="my-6">
-
-    {
-        couponList.map((item:any)=>{
-            return(
-                <div key={item.id} className="flex gap-5 my-2 items-center rounded-md bg-gray-400/30 px-5 py-3 justify-between  " >
-                  <div>
-
-                    <h1 className="font-bold"> Code = {item.code}</h1>
-                    <p > Discount = {item.discount}</p>
-                  </div>
-                    <button onClick={()=>{couponDeleteHandler(item.id)}} className="border-2 border-red-600 rounded-md px-4 py-1 text-red-600 " >
-                        Delete
-                    </button>
-                </div>
-            )
-        })
-    }
-</div>
-    
-  </div>
+      <div className="my-6">
+        {couponList.map((item: any,index:number) => {
+          return (
+            <div
+              key={index}
+              className="flex gap-5 my-2 items-center rounded-md bg-gray-400/30 px-5 py-3 justify-between  "
+            >
+              <div>
+                <h1 className="font-bold"> Code = {item.code}</h1>
+                <p> Discount = {item.discount}</p>
+              </div>
+              <button
+                onClick={() => {
+                  couponDeleteHandler(item.id);
+                }}
+                className="border-2 border-red-600 rounded-md px-4 py-1 text-red-600 "
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
